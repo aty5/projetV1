9c467e2640e5bf97846e8eef97484b4fe3dd81d2
Francis <zakouafrancis.guede2022@campus-eni.fr>
2023-03-28 11:07:51 +0200

    Début Création de sortie - pour partage Header principal

---
+++ src/Controller/GestionSortieController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Lieu;
+use App\Entity\Sortie;
+use App\Form\LieuType;
+use App\Form\SortieType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/gestion_sortie", name="gestion_sortie_")
+ */
+class GestionSortieController extends AbstractController
+{
+    /**
+     * @Route("/creer", name="creer")
+     */
+    public function creer(): Response
+    {
+        $sortie = new Sortie();
+        $sortieForm = $this->createForm(SortieType::class,$sortie);
+
+        $lieu = new Lieu();
+        $lieuForm = $this->createForm(LieuType::class,$lieu);
+
+        return $this->render('gestion_sortie/creer.html.twig',[
+            'sortieForm' => $sortieForm->createView(),
+            'lieuForm' => $lieuForm->createView(),
+        ]);
+    }
+}
---
+++ src/Form/SortieType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Sortie;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SortieType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom')
+            ->add('dateHeureDebut',DateType::class,[ 'html5' => true, 'widget' => 'single_text' ])
+            ->add('duree',DateType::class,[ 'html5' => true, 'widget' => 'single_text' ])
+            ->add('dateLimiteInscription',DateType::class,[ 'html5' => true, 'widget' => 'single_text' ])
+            ->add('nbInscriptionsMax')
+            ->add('infosSortie')
+            ->add('siteOrganisateur')
+            ->add('lieu')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Sortie::class,
+        ]);
+    }
+}
