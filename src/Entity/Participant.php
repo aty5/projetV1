ba3b1e4c91cb063085521451cd1ea8923783f47a
Kevin Arc <Kevinarcuri49150@gmail.com>
2023-03-29 20:53:29 +0200

    Utilisateur page /login , /register

--- src/Entity/Participant.php
+++ src/Entity/Participant.php
@@ -130,10 +130,11 @@ class Participant implements UserInterface, PasswordAuthenticatedUserInterface
      */
     public function getRoles(): array //se servir de l'attribut administrateur
     {
-        $roles = $this->roles;
-        // guarantee every user at least has ROLE_USER
-        $roles[] = 'ROLE_USER';
-
+        if ($this->administrateur){
+            $roles[] = 'ROLE_ADMIN';
+        }else{
+            $roles[] = 'ROLE_USER';
+        }
         return array_unique($roles);
     }
 
